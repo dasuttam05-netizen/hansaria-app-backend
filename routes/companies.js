eb73cf0e0d1e42261b184218f59b60ebf138f0ad
dasuttam05-netizen <das.uttam05@gmail.com>
2026-07-02 16:48:49 +0530

    Update companies.js

--- routes/companies.js
+++ routes/companies.js
@@ -69,10 +69,10 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
 
-    if (!isAdminUser(req.user)) {
+    if (!userHasPermission(req.user, "companies.manage")) {
       return res.status(403).json({
         error:
-          "Only admin can edit company master",
+          "You do not have permission to create companies",
       });
     }
 
@@ -121,10 +121,10 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
   try {
 
-    if (!isAdminUser(req.user)) {
+    if (!userHasPermission(req.user, "companies.manage")) {
       return res.status(403).json({
         error:
-          "Only admin can edit company master",
+          "You do not have permission to edit companies",
       });
     }
 
@@ -179,10 +179,10 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   try {
 
-    if (!isAdminUser(req.user)) {
+    if (!userHasPermission(req.user, "companies.manage")) {
       return res.status(403).json({
         error:
-          "Only admin can edit company master",
+          "You do not have permission to delete companies",
       });
     }
 
