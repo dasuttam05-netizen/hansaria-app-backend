991e2a8d481ac2a6cadb13dc76a63d0d183e59ab
dasuttam05-netizen <das.uttam05@gmail.com>
2026-07-13 17:17:29 +0530

    Update companyAccounts.js

--- routes/companyAccounts.js
+++ routes/companyAccounts.js
@@ -101,7 +101,7 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
 
-    if (!userHasPermission(req.user, "companyAccounts.manage")) {
+    if (!userHasPermission(req.user, "companyAccounts.create")) {
       return res.status(403).json({
         error:
           "You do not have permission to create company accounts",
@@ -422,7 +422,7 @@ router.post(
 router.put("/:id", async (req, res) => {
   try {
 
-    if (!userHasPermission(req.user, "companyAccounts.manage")) {
+    if (!userHasPermission(req.user, "companyAccounts.edit")) {
       return res.status(403).json({
         error:
           "You do not have permission to edit company accounts",
@@ -483,7 +483,7 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   try {
 
-    if (!userHasPermission(req.user, "companyAccounts.manage")) {
+    if (!userHasPermission(req.user, "companyAccounts.delete")) {
       return res.status(403).json({
         error:
           "You do not have permission to delete company accounts",
