63012027714668bfb52719c2ec75360aa2290fd8
dasuttam05-netizen <das.uttam05@gmail.com>
2026-07-22 15:39:36 +0530

    Update adjustmentRoutes.js

--- routes/adjustmentRoutes.js
+++ routes/adjustmentRoutes.js
@@ -184,7 +184,7 @@ router.get("/inward/report", (req, res) => {
       i.company_account_id,
       w.name AS warehouse_name,
       c.name AS company_name,
-      COALESCE(c.shortage_percent, ca.shortage_percent) AS shortage_percent,
+      COALESCE(i.shortage_percent, c.shortage_percent, ca.shortage_percent) AS shortage_percent,
       IFNULL((
         SELECT SUM(a.qty)
         FROM adjustment a
@@ -367,7 +367,7 @@ router.post("/final-save", async (req, res) => {
             i.warehouse_id,
             i.location_id,
             i.company_id,
-            COALESCE(ca.shortage_percent, c.shortage_percent) AS shortage_percent,
+            COALESCE(i.shortage_percent, c.shortage_percent, ca.shortage_percent) AS shortage_percent,
             w.location_id AS warehouse_location_id,
             IFNULL((
               SELECT SUM(a.qty)
